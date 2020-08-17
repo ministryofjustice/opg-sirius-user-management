7f6947e3cb4b906011b66e904abe17acdb0d6f5b
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-08-17 10:50:33 +0100

    Add basic Go API for validating proof-of-concept

---
+++ mock-api/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+    "log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Response struct {
+    Errors    string    `json:"errors"`
+}
+
+func main() {
+
+    router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/auth/change-password", ChangePassword).Methods(http.MethodPost, http.MethodOptions)
+	router.Use(mux.CORSMethodMiddleware(router))
+
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func ChangePassword(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+    if r.Method == http.MethodOptions {
+        return
+    }
+
+	existingPassword := r.FormValue("existingPassword")
+	password := r.FormValue("password")
+	confirmPassword := r.FormValue("confirmPassword")
+
+	errors := ""
+
+	if existingPassword == "" || password == "" || confirmPassword == "" {
+		w.WriteHeader(http.StatusBadRequest)
+        errors = "Missing required field"
+    } else if (existingPassword != "Password1") {
+		w.WriteHeader(http.StatusBadRequest)
+		errors = "Password supplied was incorrect or user is not active"
+	} else if (password != confirmPassword) {
+		w.WriteHeader(http.StatusBadRequest)
+		errors = "Confirmation did not match new password"
+	}
+
+	response := Response{Errors: errors}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+        panic(err)
+    }
+}
