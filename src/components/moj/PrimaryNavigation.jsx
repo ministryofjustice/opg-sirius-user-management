b3dbcc4d55dfaf5f3954ac24f5ccb3bd6d385dfe
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-08-18 11:45:26 +0100

    Tidy up MOJ components

--- src/components/moj/PrimaryNavigation.jsx
+++ src/components/moj/PrimaryNavigation.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-const AppNavigation = ({ items }) => (
-  <div class="moj-primary-navigation">
-    <div class="moj-primary-navigation__container">
-      <div class="moj-primary-navigation__nav">
-        <nav class="moj-primary-navigation" aria-label="Primary navigation">
-          <ul class="moj-primary-navigation__list">
-            {items.map((item) => (
-              <li class="moj-primary-navigation__item">
-                <a class="moj-primary-navigation__link" aria-current={item.active && 'page'} href={item.href}>
+const PrimaryNavigation = ({ items }) => (
+  <div className="moj-primary-navigation">
+    <div className="moj-primary-navigation__container">
+      <div className="moj-primary-navigation__nav">
+        <nav className="moj-primary-navigation" aria-label="Primary navigation">
+          <ul className="moj-primary-navigation__list">
+            {items.map((item, index) => (
+              <li key={index} className="moj-primary-navigation__item">
+                <a
+                  className="moj-primary-navigation__link"
+                  aria-current={item.active && "page"}
+                  href={item.href}
+                >
                   {item.text}
                 </a>
               </li>
@@ -20,4 +24,4 @@ const AppNavigation = ({ items }) => (
   </div>
 );
 
-export default AppNavigation;
+export default PrimaryNavigation;
