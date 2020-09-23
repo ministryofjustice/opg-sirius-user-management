7d99f0592fffee2d0220785c649807a89e0fa809
Greg Tyler <gregtyler@users.noreply.github.com>
2020-09-23 15:37:43 +0100

    Don't use New Transport font

--- build_assets.sh
+++ build_assets.sh
@@ -8,8 +8,8 @@ npx sass --load-path=. web/assets/main.scss web/static/stylesheets/all.css
 mkdir -p web/static/javascript 
 npx webpack --mode production --entry ./web/assets/main.js --output web/static/javascript/all.js
 
-mkdir -p web/static/assets/fonts
-cp node_modules/govuk-frontend/govuk/assets/fonts/* web/static/assets/fonts
+# mkdir -p web/static/assets/font
+# cp node_modules/govuk-frontend/govuk/assets/fonts/* web/static/assets/fonts
 
 mkdir -p web/static/assets/images
 cp node_modules/govuk-frontend/govuk/assets/images/* web/static/assets/images
