4a1c844cb1f2982d3e94903a7c3e1aa29045c9d9
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-10-13 16:18:41 +0100

    Tweak rules
    We want full accessibility tests, but have some wiggle room for other categories since they often report on things which aren't relevant to us.

--- docker/pa11y-ci/lighthouserc.js
+++ docker/pa11y-ci/lighthouserc.js
@@ -8,7 +8,12 @@ module.exports = {
         },
       },
       assert: {
-        preset: "lighthouse:no-pwa",
+        assertions: {
+          "categories:performance": ["warn", {"minScore": 0.9}],
+          "categories:accessibility": ["error", {"minScore": 1}],
+          "categories:best-practices": ["warn", {"minScore": 0.9}],
+          "categories:seo": ["warn", {"minScore": 0.9}],
+        },
       },
       upload: {
         target: 'temporary-public-storage',
