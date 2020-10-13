29755f78a7675a2e30e7d9a054599471200890a9
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-10-13 13:26:00 +0100

    Add auth headers to lighthouse config

--- docker/pa11y-ci/lighthouserc.js
+++ docker/pa11y-ci/lighthouserc.js
@@ -3,6 +3,7 @@ module.exports = {
       collect: {
         url: ['http://app:8888/my-details'],
         settings: {
+          extraHeaders: JSON.stringify({Cookie: 'XSRF-TOKEN=abcde; Other=other'}),
           chromeFlags: "--disable-gpu --no-sandbox",
         },
       },
