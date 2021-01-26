b00a5c66d1ee4882e10dff7db0056f146f30d976
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2021-01-26 16:20:38 +0000

    Add a response to return all required permissions
    This is to ensure that the Cypress tests run smoothly, but we can't verify it with Pact because the returned permissions object won't match.

--- internal/sirius/get_my_permissions_test.go
+++ internal/sirius/get_my_permissions_test.go
@@ -105,6 +105,91 @@ func TestPermissions(t *testing.T) {
 	}
 }
 
+func TestPermissionsIgnoredPact(t *testing.T) {
+	// We need this test to produce a specific array of permissions in the
+	// response so that Cypress tests will pass. Since Pact won't let us return
+	// multiple array entries from `dsl.EachLike` we have to write a separate
+	// test with the specific output. We call the consumer "z-append" to ensure
+	// the Pact is loaded last and overwrites the actual test of /api/permission
+	pact := &dsl.Pact{
+		Consumer:          "ignored",
+		Provider:          "ignored",
+		Host:              "localhost",
+		PactFileWriteMode: "merge",
+		LogDir:            "../../logs",
+		PactDir:           "../../pacts",
+	}
+	defer pact.Teardown()
+
+	testCases := []struct {
+		name             string
+		setup            func()
+		cookies          []*http.Cookie
+		expectedResponse PermissionSet
+		expectedError    error
+	}{
+		{
+			name: "OK",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("User exists").
+					UponReceiving("A request to get all the permissions I need").
+					WithRequest(dsl.Request{
+						Method: http.MethodGet,
+						Path:   dsl.String("/api/permission"),
+						Headers: dsl.MapMatcher{
+							"X-XSRF-TOKEN":        dsl.String("abcde"),
+							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
+							"OPG-Bypass-Membrane": dsl.String("1"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status:  http.StatusOK,
+						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
+						Body: dsl.Like(map[string]interface{}{
+							"data": map[string]interface{}{
+								"v1-users-updatetelephonenumber": map[string]interface{}{
+									"permissions": []string{"PUT"},
+								},
+								"v1-users": map[string]interface{}{
+									"permissions": []string{"PUT", "POST", "DELETE"},
+								},
+								"team": map[string]interface{}{
+									"permissions": []string{"GET", "POST", "PUT", "DELETE"},
+								},
+							},
+						}),
+					})
+			},
+			cookies: []*http.Cookie{
+				{Name: "XSRF-TOKEN", Value: "abcde"},
+				{Name: "Other", Value: "other"},
+			},
+			expectedResponse: PermissionSet{
+				"v1-users-updatetelephonenumber": PermissionGroup{Permissions: []string{"PUT"}},
+				"v1-users":                       PermissionGroup{Permissions: []string{"PUT", "POST", "DELETE"}},
+				"team":                           PermissionGroup{Permissions: []string{"GET", "POST", "PUT", "DELETE"}},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.Verify(func() error {
+				client, _ := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
+
+				myPermissions, err := client.GetMyPermissions(getContext(tc.cookies))
+				assert.Equal(t, tc.expectedResponse, myPermissions)
+				assert.Equal(t, tc.expectedError, err)
+				return nil
+			}))
+		})
+	}
+}
+
 func TestHasPermissionStatusError(t *testing.T) {
 	s := teapotServer()
 	defer s.Close()
