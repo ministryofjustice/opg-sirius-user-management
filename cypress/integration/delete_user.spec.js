32eb75720bc2600165b2c2fe32b6ce9d5a285c0e
Kate <52196833+kate-49@users.noreply.github.com>
2021-02-08 15:40:18 +0000

    Update delete_user.spec.js

--- cypress/integration/delete_user.spec.js
+++ cypress/integration/delete_user.spec.js
@@ -7,7 +7,7 @@ describe("Delete user", () => {
 
     it("allows me to delete a user", () => {
         cy.get(".govuk-body").should("contain", "Are you sure you want to delete system admin?");
-        cy.get("button[type=submit]").contains("Delete User").click();
+        cy.get("button[type=submit]").contains("Delete user").click();
 
         cy.get('a[href*="/users"]').contains('Continue').click()
         cy.url().should("include", "/users");
