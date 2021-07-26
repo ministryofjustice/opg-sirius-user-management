1dc5637d64c0af999888f7499e998f95782b6e3f
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2021-07-26 16:48:29 +0100

    adjust name for cypress test

--- cypress/integration/edit_lay_percentage.spec.js
+++ cypress/integration/edit_lay_percentage.spec.js
@@ -5,7 +5,7 @@ describe("Edit lay percentage", () => {
         cy.visit("/random-reviews/edit/lay-percentage");
     });
 
-    it("allows me to change the lay percentage value and throws an error after inputting the incorrect value", () => {
+    it("throws an error after inputting the incorrect value", () => {
         cy.get("#f-layPercentage").clear().type("200");
         cy.get("button[type=submit]").click();
         cy.get('#name-error').contains("Enter a percentage between 0 and 100 for lay cases")
