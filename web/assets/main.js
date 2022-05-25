5133b6de84dfb63a569a15a1afd72cb2e04d0341
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2022-05-25 14:42:01 +0100

    Update build script
    GOV.UK Frontend 4.1.0 changes how files should be imported
    
    #patch

--- web/assets/main.js
+++ web/assets/main.js
@@ -1,7 +1,8 @@
-import GOVUKFrontend from "govuk-frontend/govuk/all.js";
+import { initAll } from 'govuk-frontend'
 import MOJFrontend from "@ministryofjustice/frontend/moj/all.js";
 
 document.body.className = document.body.className
   ? document.body.className + " js-enabled"
   : "js-enabled";
-GOVUKFrontend.initAll();
+
+initAll();
