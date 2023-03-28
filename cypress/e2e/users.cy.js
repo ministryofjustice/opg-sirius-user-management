13e835fb153c1565c48dd12cf94f390e3c43031c
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2023-03-28 16:20:42 +0100

    SW-6384 add search for non-admin user to improve checking of team

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -3,14 +3,7 @@ describe("Users", () => {
     cy.visit("/users");
   });
 
-  it("allows me to search for a user", () => {
-    cy.get(".govuk-table").should("not.exist");
-
-    cy.get("#f-search").clear().type("admin");
-    cy.get("button[type=submit]").click();
-
-    cy.get(".govuk-table__row").should("have.length", 2);
-
+  it("allows me to search for admin user", () => {
     const expected = [
       "system admin",
       "",
@@ -18,11 +11,32 @@ describe("Users", () => {
       "Active",
       "Edit",
     ];
+    search("admin", expected);
+  });
+
+  it("allows me to search for non-admin user", () => {
+    const expected = [
+      "CardPayment User",
+      "Card Payment Team",
+      "card.payment.user@opgtest.com",
+      "Active",
+      "Edit",
+    ];
+    search("CardPayment", expected);
+  });
+
+  function search(searchTerm, expected) {
+    cy.get(".govuk-table").should("not.exist");
+
+    cy.get("#f-search").clear().type(searchTerm);
+    cy.get("button[type=submit]").click();
+
+    cy.get(".govuk-table__row").should("have.length", 2);
 
     cy.get(".govuk-table__body > .govuk-table__row")
       .children()
       .each(($el, index) => {
         cy.wrap($el).should("contain", expected[index]);
       });
-  });
+  }
 });
