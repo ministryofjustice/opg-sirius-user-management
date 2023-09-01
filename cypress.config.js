37b14b5fee201b9640b4d091c9848ef0d4bf044f
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2023-09-01 12:19:49 +0100

    Automatically run axe after each tests completes
    This gives axe prety good coverage of the app without having to maintain a separate list of URLs.
    
    #patch

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,6 @@ module.exports = defineConfig({
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:8888/',
-    supportFile: false,
+    supportFile: 'cypress/support/index.js',
   },
 })
