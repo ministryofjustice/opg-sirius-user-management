36b4673beb209dce6eb306e759203f0aad83779c
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-06-11 11:55:02 +0100

    sw-6661 add feedback tests

---
+++ internal/server/feedback_form_test.go
@@ -0,0 +1,130 @@
+package server
+
+import (
+	"github.com/ministryofjustice/opg-sirius-user-management/internal/model"
+	"github.com/ministryofjustice/opg-sirius-user-management/internal/sirius"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type mockFeedbackFormClient struct {
+	count   int
+	lastCtx sirius.Context
+	err     error
+	form    model.FeedbackForm
+}
+
+func (m *mockFeedbackFormClient) AddFeedback(ctx sirius.Context, form model.FeedbackForm) error {
+	m.count += 1
+	m.lastCtx = ctx
+
+	return m.err
+}
+
+func TestGetFeedbackForm(t *testing.T) {
+	assert := assert.New(t)
+
+	client := &mockFeedbackFormClient{}
+	template := &mockTemplate{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/path", nil)
+
+	handler := feedbackForm(client, template)
+	err := handler(sirius.PermissionSet{}, w, r)
+	assert.Nil(err)
+
+	resp := w.Result()
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	assert.Equal(1, template.count)
+	assert.Equal(feedbackFormVars{
+		Path: "/feedback",
+	}, template.lastVars)
+}
+
+func TestConfirmPostFeedbackForm(t *testing.T) {
+	assert := assert.New(t)
+
+	client := &mockFeedbackFormClient{
+		form: model.FeedbackForm{
+			IsSupervisionFeedback: true,
+			Name:                  "",
+			Email:                 "",
+			CaseNumber:            "",
+			Message:               "Im not happy with this service",
+		},
+	}
+	template := &mockTemplate{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/feedback-form", strings.NewReader("more-detail=Im not happy with this service"))
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	err := feedbackForm(client, template)(sirius.PermissionSet{}, w, r)
+	assert.Nil(err)
+}
+
+func TestGetFeedbackFormMethodNotAllowed(t *testing.T) {
+	assert := assert.New(t)
+
+	client := &mockFeedbackFormClient{
+		err: StatusError(http.StatusMethodNotAllowed),
+		form: model.FeedbackForm{
+			Message: "test",
+		},
+	}
+	template := &mockTemplate{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/feedback-form", strings.NewReader("more-detail=Im not happy with this service"))
+
+	handler := feedbackForm(client, template)
+	err := handler(sirius.PermissionSet{}, w, r)
+
+	assert.Equal(StatusError(http.StatusMethodNotAllowed), err)
+}
+
+func TestGetFeedbackFormReturnsPermissionsErrorsIfInputTooLong(t *testing.T) {
+	assert := assert.New(t)
+
+	client := &mockFeedbackFormClient{}
+	client.err = sirius.ValidationError{
+		Message: "stringLengthTooLong",
+	}
+	template := &mockTemplate{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/feedback-form", strings.NewReader(""))
+
+	handler := feedbackForm(client, template)
+	err := handler(sirius.PermissionSet{}, w, r)
+
+	assert.Equal(sirius.ValidationError{
+		Message: "stringLengthTooLong",
+		Errors:  nil,
+	}, err)
+}
+
+func TestGetFeedbackFormReturnsPermissionsErrorsIfNoFeedback(t *testing.T) {
+	assert := assert.New(t)
+
+	client := &mockFeedbackFormClient{}
+	client.err = sirius.ValidationError{
+		Message: "isEmpty",
+	}
+	template := &mockTemplate{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/feedback-form", strings.NewReader(""))
+
+	handler := feedbackForm(client, template)
+	err := handler(sirius.PermissionSet{}, w, r)
+
+	assert.Equal(sirius.ValidationError{
+		Message: "isEmpty",
+		Errors:  nil,
+	}, err)
+}
