ad58a4f2a5212859055f475ebc80e8060f6aa4ae
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-06-11 11:55:02 +0100

    sw-6661 add feedback tests

--- web/assets/main.js
+++ web/assets/main.js
@@ -2,14 +2,9 @@ import * as GOVUKFrontend from "govuk-frontend";
 import MOJFrontend from "@ministryofjustice/frontend/moj/all.js";
 import CloseTab from "./close-tab";
 
-//document.body.className = document.body.className
-//  ? document.body.className + " js-enabled"
-//  : "js-enabled";
-
 const closeTab = document.querySelectorAll('[data-module="moj-close-tab"]');
 closeTab.forEach(function (closeTab) {
   new CloseTab(closeTab);
 });
-
 
 GOVUKFrontend.initAll();
