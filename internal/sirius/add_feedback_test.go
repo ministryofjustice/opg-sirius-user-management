0c2b28b0f56a77d444c58cc3964e978bc788ef2f
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-06-13 14:01:55 +0100

    sw-6661 try happy pact test

--- internal/sirius/add_feedback_test.go
+++ internal/sirius/add_feedback_test.go
@@ -1,12 +1,18 @@
 package sirius
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/ministryofjustice/opg-sirius-user-management/internal/mocks"
 	"github.com/ministryofjustice/opg-sirius-user-management/internal/model"
 	"github.com/pact-foundation/pact-go/dsl"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -77,118 +83,187 @@ func TestAddFeedback(t *testing.T) {
 	}
 }
 
-//func TestAddFeedbackCanPost(t *testing.T) {
-//	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
-//	defer svr.Close()
-//
-//	client, _ := NewClient(http.DefaultClient, svr.URL)
-//
-//	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
-//		IsSupervisionFeedback: true,
-//		Name:                  "Toad",
-//		Email:                 "toad@toadhall.com",
-//		CaseNumber:            "123",
-//		Message:               "content",
-//	})
-//	assert.Nil(t, err)
-//}
-//
-//func TestAddFeedbackCanHandleValidationError(t *testing.T) {
-//	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		w.WriteHeader(http.StatusBadRequest)
-//	}))
-//	defer svr.Close()
-//
-//	client, _ := NewClient(http.DefaultClient, svr.URL)
-//
-//	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
-//		IsSupervisionFeedback: true,
-//		Name:                  "Toad",
-//		Email:                 "toad@toadhall.com",
-//		CaseNumber:            "123",
-//		Message:               "content",
-//	})
-//	assert.Equal(t, StatusError{
-//		Code:   http.StatusBadRequest,
-//		URL:    svr.URL + "/api/supervision-feedback",
-//		Method: http.MethodPost,
-//	}, err)
-//}
-//
-//func TestAddFeedbackCanHandleUnauthorizedError(t *testing.T) {
-//	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		w.WriteHeader(http.StatusUnauthorized)
-//	}))
-//	defer svr.Close()
-//
-//	client, _ := NewClient(http.DefaultClient, svr.URL)
-//
-//	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
-//		IsSupervisionFeedback: true,
-//		Name:                  "Toad",
-//		Email:                 "toad@toadhall.com",
-//		CaseNumber:            "123",
-//		Message:               "content",
-//	})
-//	assert.Equal(t, ClientError("unauthorized"), err)
-//}
-//
-//func TestGetCaseloadListCanThrow500Error(t *testing.T) {
-//	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		w.WriteHeader(http.StatusInternalServerError)
-//	}))
-//	defer svr.Close()
-//
-//	client, _ := NewClient(http.DefaultClient, svr.URL)
-//
-//	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
-//		IsSupervisionFeedback: true,
-//		Name:                  "Toad",
-//		Email:                 "toad@toadhall.com",
-//		CaseNumber:            "123",
-//		Message:               "feedback",
-//	})
-//	assert.Equal(t, StatusError{
-//		Code:   http.StatusInternalServerError,
-//		URL:    svr.URL + "/api/supervision-feedback",
-//		Method: http.MethodPost,
-//	}, err)
-//}
-//
-//func TestAddFeedbackIsEmptyValidationError(t *testing.T) {
-//	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
-//	defer svr.Close()
-//
-//	client, _ := NewClient(http.DefaultClient, svr.URL)
-//
-//	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
-//		IsSupervisionFeedback: true,
-//		Name:                  "Toad",
-//		Email:                 "toad@toadhall.com",
-//		CaseNumber:            "123",
-//		Message:               "",
-//	})
-//	assert.Equal(t, ValidationError{
-//		Message: "isEmpty",
-//		Errors:  nil,
-//	}, err)
-//}
-//
-//func TestAddFeedbackStringTooLongValidationError(t *testing.T) {
-//	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
-//	defer svr.Close()
-//
-//	client, _ := NewClient(http.DefaultClient, svr.URL)
-//
-//	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
-//		IsSupervisionFeedback: true,
-//		Name:                  "Toad",
-//		Email:                 "toad@toadhall.com",
-//		CaseNumber:            "123",
-//		Message:               strings.Repeat("a", 901),
-//	})
-//	assert.Equal(t, ValidationError{
-//		Message: "stringLengthTooLong",
-//		Errors:  nil,
-//	}, err)
-//}
+func TestAddFeedbackCanPost(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	defer svr.Close()
+
+	client, _ := NewClient(http.DefaultClient, svr.URL)
+
+	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
+		IsSupervisionFeedback: true,
+		Name:                  "Toad",
+		Email:                 "toad@toadhall.com",
+		CaseNumber:            "123",
+		Message:               "content",
+	})
+	assert.Nil(t, err)
+}
+
+func TestAddFeedbackCanHandleBadRequest(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusBadRequest)
+	}))
+	defer svr.Close()
+
+	client, _ := NewClient(http.DefaultClient, svr.URL)
+
+	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
+		IsSupervisionFeedback: true,
+		Name:                  "Toad",
+		Email:                 "toad@toadhall.com",
+		CaseNumber:            "123",
+		Message:               "content",
+	})
+	assert.Equal(t, StatusError{
+		Code:   http.StatusBadRequest,
+		URL:    svr.URL + "/api/supervision-feedback",
+		Method: http.MethodPost,
+	}, err)
+}
+
+func TestAddFeedbackCanHandleUnauthorizedError(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusUnauthorized)
+	}))
+	defer svr.Close()
+
+	client, _ := NewClient(http.DefaultClient, svr.URL)
+
+	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
+		IsSupervisionFeedback: true,
+		Name:                  "Toad",
+		Email:                 "toad@toadhall.com",
+		CaseNumber:            "123",
+		Message:               "content",
+	})
+	assert.Equal(t, ClientError("unauthorized"), err)
+}
+
+func TestGetCaseloadListCanThrow500Error(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer svr.Close()
+
+	client, _ := NewClient(http.DefaultClient, svr.URL)
+
+	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
+		IsSupervisionFeedback: true,
+		Name:                  "Toad",
+		Email:                 "toad@toadhall.com",
+		CaseNumber:            "123",
+		Message:               "feedback",
+	})
+
+	assert.Equal(t, StatusError{
+		Code:   http.StatusInternalServerError,
+		URL:    svr.URL + "/api/supervision-feedback",
+		Method: http.MethodPost,
+	}, err)
+
+}
+
+func TestCanThrowReqErrors(t *testing.T) {
+	jsonResponse := `{"detail": "Could not post to Slack"}`
+
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	}))
+	defer svr.Close()
+
+	client, _ := NewClient(http.DefaultClient, svr.URL)
+	r := io.NopCloser(bytes.NewReader([]byte(jsonResponse)))
+
+	mocks.GetDoFunc = func(rq *http.Request) (*http.Response, error) {
+
+		err := json.NewDecoder(rq.Body)
+		assert.Nil(t, err)
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       r,
+		}, nil
+	}
+
+	err := client.AddFeedback(getContext(nil), model.FeedbackForm{
+		IsSupervisionFeedback: true,
+		Name:                  "",
+		Email:                 "",
+		CaseNumber:            "",
+		Message:               "feedback message",
+	})
+	assert.Equal(t, nil, err)
+}
+
+func TestAddFeedbackIsEmptyValidationError(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	defer svr.Close()
+
+	client, _ := NewClient(http.DefaultClient, svr.URL)
+
+	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
+		IsSupervisionFeedback: true,
+		Name:                  "Toad",
+		Email:                 "toad@toadhall.com",
+		CaseNumber:            "123",
+		Message:               "",
+	})
+
+	assert.Equal(t, ValidationError{
+		Message: "isEmpty",
+	}, err)
+}
+
+func TestAddFeedbackStringTooLongValidationError(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	defer svr.Close()
+
+	client, _ := NewClient(http.DefaultClient, svr.URL)
+
+	err := client.AddFeedback(Context{Context: context.Background()}, model.FeedbackForm{
+		IsSupervisionFeedback: true,
+		Name:                  "Toad",
+		Email:                 "toad@toadhall.com",
+		CaseNumber:            "123",
+		Message:               strings.Repeat("a", 901),
+	})
+	assert.Equal(t, ValidationError{
+		Message: "stringLengthTooLong",
+	}, err)
+}
+
+func TestValidationErrorUnmarshalled(t *testing.T) {
+	mockClient := &mocks.MockClient{}
+	client, _ := NewClient(mockClient, "http://localhost:3000")
+
+	jsonResponse := `{
+    "validation_errors": {
+        "feedback": {
+            "isEmpty": "Message is required and can not be empty"
+        }
+    },
+    "type": "http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html",
+    "title": "Bad Request",
+    "status": 400,
+    "detail": "Payload failed validation"
+	}`
+	r := io.NopCloser(bytes.NewReader([]byte(jsonResponse)))
+
+	mocks.GetDoFunc = func(rq *http.Request) (*http.Response, error) {
+		return &http.Response{
+			StatusCode: 400,
+			Body:       r,
+		}, nil
+	}
+
+	expectedResponse := ValidationError{Message: "", Errors: ValidationErrors{"feedback": map[string]string{"isEmpty": "Message is required and can not be empty"}}}
+
+	err := client.AddFeedback(getContext(nil), model.FeedbackForm{
+		IsSupervisionFeedback: true,
+		Name:                  "",
+		Email:                 "",
+		CaseNumber:            "",
+		Message:               "feedback message",
+	})
+	assert.Equal(t, expectedResponse, err)
+}
